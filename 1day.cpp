4ba319d9b8efb355723b703cd5d88309c5913b36
krishgupta57 <krishgupta7898@gmail.com>
2025-08-21 07:19:47 +0530

    Basic

---
+++ 1day.cpp
@@ -0,0 +1,19 @@
+
+
+#include<iostream>
+  using namespace std;
+  int main()
+  {
+      int x=10;
+      int y=20;
+      int z=x+y;
+      char c='kfjjfj';
+      int a=145;
+      char A='A';
+      cout<<"The numeric value of A="<<z<<endl;
+      cout<<"The sum of x and y="<<z;
+      cout<<"charcter="<<c<<endl;
+      return 0;
+
+  }
+

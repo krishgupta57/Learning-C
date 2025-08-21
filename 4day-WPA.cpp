d32d636da0b3bbaf34f7cabbe463d09c30d6bdfd
krishgupta57 <krishgupta7898@gmail.com>
2025-08-21 07:27:24 +0530

    WAP

---
+++ 4day-WPA.cpp
@@ -0,0 +1,45 @@
+//WAP to find maximum nunmber from 3 given input?
+
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    /*cout<<"enter first no. - "<<endl;
+    cout<<"enter second no. - "<<endl;
+    cout<<"enter third no. - "<<endl;
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a>b>c) cout<<"first is max";
+    else if(b>c>a) cout<<"second is max";
+    else cout<<"third is max";*/
+
+    // roots of quadratic equation
+
+    /*int a,b,c;
+    float d,r1,r2,d1;
+    cout<<"enter the coff of x^2, x and x^0"<<endl;
+    cin>>a>>b>>c;
+    d=b*b-4*a*c;
+    d1=sqrt(d);
+    r1=(-b+d1)/2*a;
+    r2=(-b-d1)/2*a;
+    cout<<"root1"<<r1<<endl;
+    cout<<"root2"<<r2<<endl;*/
+
+    int a=7;
+    int b=4;
+    int c=a&b;
+    cout<<c<<endl;
+
+
+
+
+
+
+    return 0;
+
+
+}

a0d839be7561c607281a60afc601dbe1a32624b7
krishgupta57 <krishgupta7898@gmail.com>
2025-08-21 07:28:49 +0530

    Bitwise Operator

---
+++ 5day-Bitwise.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /*cout<<"Bitwise operator"<<endl;
+    int a=7;
+    int b=4;
+    int bit_ands=a&b;
+    cout<<"bitwise and operator between a and b = "<<bit_ands<<endl;
+    int bit_ors=a|b;
+    cout<<"bitwise or operator between a and b = "<<bit_ors<<endl;
+    int bit_zors=a^b;
+    cout<<"bitwise zor operator between a and b = "<<bit_zors<<endl;
+    int bit_nots=~a;
+    cout<<"bitwise not operator between a and b = "<<bit_nots<<endl;*/
+
+    /*int i=1;  //initialization of while loop
+    while(i<=10)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+    cout<<"end value="<<i<<endl;
+
+
+    int j=10;  //initialization of while loop
+    while(j>=1)
+    {
+        cout<<j<<endl;
+        j--;
+    }
+    cout<<"end value="<<j<<endl;*/
+
+    int k=1;
+    int add=0;
+    while(k<=10)
+    {
+        add=add+k;
+        k++;
+    }
+    cout<<"end value="<<add<<endl;
+
+
+    return 0;
+}
